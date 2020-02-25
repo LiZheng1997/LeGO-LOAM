304baa06e506bb04f459b04ddf0e9db58cd0e699
LiZheng1997 <1429053840@qq.com>
2020-02-25 17:05:42 +0000

    refined the Main.cpp

--- LeGO-LOAM/src/Main.cpp
+++ LeGO-LOAM/src/Main.cpp
@@ -1,6 +1,15 @@
 #include "utility.h"
+#include "imageProjection.h"
+#include "featureAssociation.h"
 
 int main(int argc, char** argv){
+
+    double _time_new_segmented_cloud;
+    double _time_new_segmented_cloudInfo;
+
+    bool _new_segmented_cloud;
+    bool _new_segmented_cloudInfo;
+    bool _new_outlier_cloud;
 
     pcl::PointCloud<PointType>::Ptr _outlier_cloud;
 
@@ -10,32 +19,88 @@ int main(int argc, char** argv){
 
     pcl::PointCloud<PointType>::Ptr _laser_cloudIn;
 
+
     // ros::init(argc, argv, "lego_loam");
     
-    ImageProjection IP;
-
-    _laser_cloudIn  = IP::laserCloudIn; 
-
-    // 2. Start and end angle of a scan
-    IP::findStartEndAngle();
-    // 3. Range image projection
-    IP::projectPointCloud();
-    // 4. Mark ground points
-    IP::groundRemoval();
-    // 5. Point cloud segmentation
-    IP::cloudSegmentation();
-    // 6. Publish all clouds
-    IP::publishCloud();
-    // 7. Reset parameters for next iteration
-    IP::resetParameters();
-
-    _outlier_cloud = IP::outlierCloud;
-
-    _segmented_cloud = IP::segmentedCloud;
-
-    _seg_msg = IP::segMsg;
+    ImageProjection IP; //initiate a ImageProjection instance
 
     
+
+    void imageProjectionM(){
+        _laser_cloudIn  = IP::laserCloudIn; //original point cloud data from ROS message
+
+        // 2. Start and end angle of a scan
+        IP::findStartEndAngle();
+        // 3. Range image projection
+        IP::projectPointCloud();
+        // 4. Mark ground points
+        IP::groundRemoval();
+        // 5. Point cloud segmentation
+        IP::cloudSegmentation();
+        // 6. Publish all clouds
+        IP::publishCloud();
+        // 7. Reset parameters for next iteration
+        IP::resetParameters();
+
+        _outlier_cloud = IP::outlierCloud;
+
+        _segmented_cloud = IP::segmentedCloud;
+
+        _seg_msg = IP::segMsg;
+    }
+    
+    void featureAssociationM(){
+
+        FeatureAssociation FA;
+
+        if (newSegmentedCloud && newSegmentedCloudInfo && newOutlierCloud &&
+            std::abs(_time_new_segmented_cloudInfo - _time_new_segmented_cloud) < 0.05 &&
+            std::abs(_time_new_segmented_cloud - _time_new_segmented_cloud) < 0.05){
+
+            _new_segmented_cloud = false;
+            _new_segmented_cloudInfo = false;
+            _new_outlier_cloud = false;
+        }else{
+            return;
+        }
+        /**
+        	1. Feature Extraction
+        */
+        adjustDistortion();
+
+        calculateSmoothness();
+
+        markOccludedPoints();
+
+        extractFeatures();
+
+        publishCloud(); // cloud for visualization
+	
+        /**
+		2. Feature Association
+        */
+        if (!systemInitedLM) {
+            checkSystemInitialization();
+            return;
+        }
+
+        updateInitialGuess();
+
+        updateTransformation();
+
+        integrateTransformation();
+
+        publishOdometry();
+
+        publishCloudsLast(); // cloud to mapOptimization
+    
+    }
+
+
+
+
+
+
 
     // ROS_INFO("\033[1;32m---->\033[0m Image Projection Started.");
 
