c88ed89d113d12fb089f546cb281f0f029bb6300
LiZheng1997 <1429053840@qq.com>
2020-02-25 17:05:09 +0000

    revised the featureAssociation.cpp and add a head

--- LeGO-LOAM/src/featureAssociation.cpp
+++ LeGO-LOAM/src/featureAssociation.cpp
@@ -33,158 +33,11 @@
 //      IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS). October 2018.
 
 #include "utility.h"
-
-class FeatureAssociation{
-
-private:
-
-	ros::NodeHandle nh;
-
-    ros::Subscriber subLaserCloud;
-    ros::Subscriber subLaserCloudInfo;
-    ros::Subscriber subOutlierCloud;
-    ros::Subscriber subImu;
-
-    ros::Publisher pubCornerPointsSharp;
-    ros::Publisher pubCornerPointsLessSharp;
-    ros::Publisher pubSurfPointsFlat;
-    ros::Publisher pubSurfPointsLessFlat;
-
-    pcl::PointCloud<PointType>::Ptr segmentedCloud;
-    pcl::PointCloud<PointType>::Ptr outlierCloud;
-
-    pcl::PointCloud<PointType>::Ptr cornerPointsSharp;
-    pcl::PointCloud<PointType>::Ptr cornerPointsLessSharp;
-    pcl::PointCloud<PointType>::Ptr surfPointsFlat;
-    pcl::PointCloud<PointType>::Ptr surfPointsLessFlat;
-
-    pcl::PointCloud<PointType>::Ptr surfPointsLessFlatScan;
-    pcl::PointCloud<PointType>::Ptr surfPointsLessFlatScanDS;
-
-    pcl::VoxelGrid<PointType> downSizeFilter;
-
-    double timeScanCur;
-    double timeNewSegmentedCloud;
-    double timeNewSegmentedCloudInfo;
-    double timeNewOutlierCloud;
-
-    bool newSegmentedCloud;
-    bool newSegmentedCloudInfo;
-    bool newOutlierCloud;
-
-    cloud_msgs::cloud_info segInfo;
-    std_msgs::Header cloudHeader;
-
-    int systemInitCount;
-    bool systemInited;
-
-    std::vector<smoothness_t> cloudSmoothness;
-    float cloudCurvature[N_SCAN*Horizon_SCAN];
-    int cloudNeighborPicked[N_SCAN*Horizon_SCAN];
-    int cloudLabel[N_SCAN*Horizon_SCAN];
-
-    int imuPointerFront;
-    int imuPointerLast;
-    int imuPointerLastIteration;
-
-    float imuRollStart, imuPitchStart, imuYawStart;
-    float cosImuRollStart, cosImuPitchStart, cosImuYawStart, sinImuRollStart, sinImuPitchStart, sinImuYawStart;
-    float imuRollCur, imuPitchCur, imuYawCur;
-
-    float imuVeloXStart, imuVeloYStart, imuVeloZStart;
-    float imuShiftXStart, imuShiftYStart, imuShiftZStart;
-
-    float imuVeloXCur, imuVeloYCur, imuVeloZCur;
-    float imuShiftXCur, imuShiftYCur, imuShiftZCur;
-
-    float imuShiftFromStartXCur, imuShiftFromStartYCur, imuShiftFromStartZCur;
-    float imuVeloFromStartXCur, imuVeloFromStartYCur, imuVeloFromStartZCur;
-
-    float imuAngularRotationXCur, imuAngularRotationYCur, imuAngularRotationZCur;
-    float imuAngularRotationXLast, imuAngularRotationYLast, imuAngularRotationZLast;
-    float imuAngularFromStartX, imuAngularFromStartY, imuAngularFromStartZ;
-
-    double imuTime[imuQueLength];
-    float imuRoll[imuQueLength];
-    float imuPitch[imuQueLength];
-    float imuYaw[imuQueLength];
-
-    float imuAccX[imuQueLength];
-    float imuAccY[imuQueLength];
-    float imuAccZ[imuQueLength];
-
-    float imuVeloX[imuQueLength];
-    float imuVeloY[imuQueLength];
-    float imuVeloZ[imuQueLength];
-
-    float imuShiftX[imuQueLength];
-    float imuShiftY[imuQueLength];
-    float imuShiftZ[imuQueLength];
-
-    float imuAngularVeloX[imuQueLength];
-    float imuAngularVeloY[imuQueLength];
-    float imuAngularVeloZ[imuQueLength];
-
-    float imuAngularRotationX[imuQueLength];
-    float imuAngularRotationY[imuQueLength];
-    float imuAngularRotationZ[imuQueLength];
+#include "featureAssociation.h"
 
 
-
-    ros::Publisher pubLaserCloudCornerLast;
-    ros::Publisher pubLaserCloudSurfLast;
-    ros::Publisher pubLaserOdometry;
-    ros::Publisher pubOutlierCloudLast;
-
-    int skipFrameNum;
-    bool systemInitedLM;
-
-    int laserCloudCornerLastNum;
-    int laserCloudSurfLastNum;
-
-    int pointSelCornerInd[N_SCAN*Horizon_SCAN];
-    float pointSearchCornerInd1[N_SCAN*Horizon_SCAN];
-    float pointSearchCornerInd2[N_SCAN*Horizon_SCAN];
-
-    int pointSelSurfInd[N_SCAN*Horizon_SCAN];
-    float pointSearchSurfInd1[N_SCAN*Horizon_SCAN];
-    float pointSearchSurfInd2[N_SCAN*Horizon_SCAN];
-    float pointSearchSurfInd3[N_SCAN*Horizon_SCAN];
-
-    float transformCur[6];
-    float transformSum[6];
-
-    float imuRollLast, imuPitchLast, imuYawLast;
-    float imuShiftFromStartX, imuShiftFromStartY, imuShiftFromStartZ;
-    float imuVeloFromStartX, imuVeloFromStartY, imuVeloFromStartZ;
-
-    pcl::PointCloud<PointType>::Ptr laserCloudCornerLast;
-    pcl::PointCloud<PointType>::Ptr laserCloudSurfLast;
-    pcl::PointCloud<PointType>::Ptr laserCloudOri;
-    pcl::PointCloud<PointType>::Ptr coeffSel;
-
-    pcl::KdTreeFLANN<PointType>::Ptr kdtreeCornerLast;
-    pcl::KdTreeFLANN<PointType>::Ptr kdtreeSurfLast;
-
-    std::vector<int> pointSearchInd;
-    std::vector<float> pointSearchSqDis;
-
-    PointType pointOri, pointSel, tripod1, tripod2, tripod3, pointProj, coeff;
-
-    nav_msgs::Odometry laserOdometry;
-
-    tf::TransformBroadcaster tfBroadcaster;
-    tf::StampedTransform laserOdometryTrans;
-
-    bool isDegenerate;
-    cv::Mat matP;
-
-    int frameCount;
-
-public:
-
-    FeatureAssociation():
-        nh("~")
+    FeatureAssociation::FeatureAssociation(ros::NodeHandle &node)
+        : nh(node)
         {
 
         subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/segmented_cloud", 1, &FeatureAssociation::laserCloudHandler, this);
@@ -1804,47 +1657,7 @@ public:
     void runFeatureAssociation()
     {
 
-        if (newSegmentedCloud && newSegmentedCloudInfo && newOutlierCloud &&
-            std::abs(timeNewSegmentedCloudInfo - timeNewSegmentedCloud) < 0.05 &&
-            std::abs(timeNewOutlierCloud - timeNewSegmentedCloud) < 0.05){
-
-            newSegmentedCloud = false;
-            newSegmentedCloudInfo = false;
-            newOutlierCloud = false;
-        }else{
-            return;
-        }
-        /**
-        	1. Feature Extraction
-        */
-        adjustDistortion();
-
-        calculateSmoothness();
-
-        markOccludedPoints();
-
-        extractFeatures();
-
-        publishCloud(); // cloud for visualization
-	
-        /**
-		2. Feature Association
-        */
-        if (!systemInitedLM) {
-            checkSystemInitialization();
-            return;
-        }
-
-        updateInitialGuess();
-
-        updateTransformation();
-
-        integrateTransformation();
-
-        publishOdometry();
-
-        publishCloudsLast(); // cloud to mapOptimization
-    }
+        
 };
 
 
@@ -1856,7 +1669,7 @@ int main(int argc, char** argv)
 
     ROS_INFO("\033[1;32m---->\033[0m Feature Association Started.");
 
-    FeatureAssociation FA;
+    
 
     ros::Rate rate(200);
     while (ros::ok())
